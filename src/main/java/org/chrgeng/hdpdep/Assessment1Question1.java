e3bab23ae1191ab96c8d5ba4b2ab12ae81aa044f
CHRGENG <CHRGENG@C415M5CG5524D06.detss.corpintra.net>
2017-09-12 12:49:02 +0200

    added Assessment1 Source code

---
+++ src/main/java/org/chrgeng/hdpdep/Assessment1Question1.java
@@ -0,0 +1,27 @@
+package org.chrgeng.hdpdep;
+
+import java.util.Arrays;
+
+public class Assessment1Question1 {
+	/* */
+	
+	public void twoDimensions(int[][] numbers){
+	    for (int i = 0; i < numbers.length - 1; i++){
+	        for (int j = 0; j < numbers[i].length - 1; j++){
+	            if (numbers[i][j] < numbers[i][j + 1]){
+	                numbers[i][j] = numbers[i][j] + numbers[i + 1][j];
+	                numbers[i + 1][j] = numbers[i + 1][j] / 2;
+	            }
+	        }
+	    }
+	}
+	
+	
+	public static void printTwoDimensions(int[][]number) {
+ 		for (int i = 0; i < number.length; i++) {
+        	System.out.println(Arrays.toString(number[i]));
+        }
+ 	}
+	
+	
+}
