73d0b639c3e3aab785f2835d1484f0b663590fe1
Geng, Christian (415) <christian.geng@daimler.com>
2018-01-16 20:25:41 +0100

    Selection Sort bad version

---
+++ src/main/java/sorting/Main.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package sorting;
+
+import org.chrgeng.hdpdep.Assignment2.Assessment2Question6;
+import org.chrgeng.hdpdep.Assignment3.Assessment3Question5;
+
+/**
+ * @author christian
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		//Number firstNum = new Number();
+        //System.out.println(firstNum.toString());
+		//System.out.println(/* missing code */);
+		//Assessment2Question1 test = new Assessment2Question1();
+		//Assessment2Question2 test = new Assessment2Question2();
+		//Assessment2Question4 test = new Assessment2Question4();
+		//Assessment2Question6 test = new Assessment2Question6();
+//		Number firstN;um = new Number();
+//		System.out.println("main:");
+//		System.out.println( firstNum );
+//		System.out.println( firstNum.toString() );
+		//Assessment3Question5 test = new Assessment3Question5();
+		int[] lst = {4,9,7,1,3,6,5};
+		SelectionSort ss = new SelectionSort(lst);
+		
+		
+	}	
+
+}
